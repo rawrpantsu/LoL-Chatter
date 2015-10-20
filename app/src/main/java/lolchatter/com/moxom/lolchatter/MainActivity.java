39823746a84f37cf9a7b4598cfa05501ec3afd6d
Marcin <xcrazibranchx@gmail.com>
2015-10-19 23:48:22 -0400

    -added LoL XMPP-Chat-Library

--- app/src/main/java/lolchatter/com/moxom/lolchatter/MainActivity.java
+++ app/src/main/java/lolchatter/com/moxom/lolchatter/MainActivity.java
@@ -1,5 +1,13 @@
 package lolchatter.com.moxom.lolchatter;
 
+        import com.github.theholywaffle.lolchatapi.ChatServer;
+        import com.github.theholywaffle.lolchatapi.FriendRequestPolicy;
+        import com.github.theholywaffle.lolchatapi.LolChat;
+        import com.github.theholywaffle.lolchatapi.listeners.ChatListener;
+        import com.github.theholywaffle.lolchatapi.riotapi.RateLimit;
+        import com.github.theholywaffle.lolchatapi.riotapi.RiotApiKey;
+        import com.github.theholywaffle.lolchatapi.wrapper.Friend;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -86,6 +94,34 @@ public class MainActivity extends AppCompatActivity
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
+        final LolChat api = new LolChat(ChatServer.EUW, FriendRequestPolicy.ACCEPT_ALL, new RiotApiKey("RIOT-API-KEY", RateLimit.DEFAULT));
+        if (api.login("myusername", "mypassword")) {
+
+            // Example 1: Send Chat Message to all your friends
+            for (Friend f : api.getFriends()) {
+                f.sendMessage("Hello " + f.getName());
+            }
+
+            // Example 2: Send Chat Message to all your friends and wait for an
+            // response
+            for (Friend f : api.getFriends()) {
+                f.sendMessage("Hello " + f.getName(), new ChatListener() {
+
+                    @Override
+                    public void onMessage(Friend friend, String message) {
+                        System.out.println("Friend " + friend.getName()
+                                + " responded to my Hello World!");
+                    }
+                });
+            }
+
+            // Example3: Send Chat Message to an specific friend
+            Friend f = api.getFriendByName("Dyrus");
+            if (f != null && f.isOnline()) {
+                f.sendMessage("Hi, I'm your biggest fan!");
+            }
+        }
 
 
     }
