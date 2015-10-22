02678c43d20cb584d18a0b7e669e039592e3a515
Marcin <xcrazibranchx@gmail.com>
2015-10-21 23:47:00 -0400

    -resolved compiler errors -cleaned up connection code

--- app/src/main/java/lolchatter/com/moxom/lolchatter/MainActivity.java
+++ app/src/main/java/lolchatter/com/moxom/lolchatter/MainActivity.java
@@ -1,65 +1,39 @@
 package lolchatter.com.moxom.lolchatter;
 
-        import android.app.ProgressDialog;
-        import android.os.AsyncTask;
-        import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-        import android.util.Log;
-        import android.view.LayoutInflater;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-//fragment imports
-
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
+import org.jivesoftware.smack.AbstractXMPPConnection;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 
-        import org.jivesoftware.smack.AbstractXMPPConnection;
-        import org.jivesoftware.smack.ConnectionConfiguration;
-        import org.jivesoftware.smack.SmackException;
-        import org.jivesoftware.smack.XMPPException;
-        import org.jivesoftware.smack.tcp.XMPPTCPConnection;
-        import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import java.io.IOException;
 
-        import java.io.IOException;
-        import java.security.KeyManagementException;
-        import java.security.NoSuchAlgorithmException;
-        import java.security.SecureRandom;
+import javax.net.ssl.SSLSocketFactory;
 
-        import javax.net.ssl.SSLContext;
-        import javax.net.ssl.SSLSocketFactory;
+//fragment imports
 
 
 public class MainActivity extends AppCompatActivity
@@ -202,6 +176,40 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -240,44 +248,9 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-
     private class AsyncCaller extends AsyncTask<Void, Void, Void> {
         ProgressDialog pdLoading = new ProgressDialog(MainActivity.this);
+        AbstractXMPPConnection conn2;
 
         @Override
         protected void onPreExecute() {
@@ -294,76 +267,30 @@ public class MainActivity extends AppCompatActivity
 
             try {
 
-
-             /*
-                 ConnectionConfiguration config = new ConnectionConfiguration("chat.na2.lol.riotgames.com", 5223,);
-                config.setSecurityMode(ConnectionConfiguration.SecurityMode.enabled);
-                config.setSocketFactory(SSLSocketFactory.getDefault());
-                config.setCompressionEnabled(true);
-                connection = new XMPPTCPConnection(config);
-*/
-/*
-
-
-                int abc = 5;
                 XMPPTCPConnectionConfiguration config2 = XMPPTCPConnectionConfiguration.builder()
-                        .setUsernameAndPassword("username", "AIR_password")
+                        .setUsernameAndPassword("username", "AIR_" + "password")
                         .setResource("xiff")
                         .setHost("chat.na2.lol.riotgames.com")
                         .setPort(5223)
                         .setServiceName("pvp.net")
-                                .setSecurityMode(ConnectionConfiguration.SecurityMode.required)
-                      //  .setSecurityMode(ConnectionConfiguration.SecurityMode.ifpossible)
-                       .setSocketFactory(SSLSocketFactory.getDefault())
+                                //  .setSecurityMode(ConnectionConfiguration.SecurityMode.required)
+                                //  .setSecurityMode(ConnectionConfiguration.SecurityMode.ifpossible)
+                        .setSocketFactory(SSLSocketFactory.getDefault())
                         .setCompressionEnabled(true)
                         .build();
 
-                AbstractXMPPConnection conn2 = new XMPPTCPConnection(config2);
+                conn2 = new XMPPTCPConnection(config2);
                 conn2.connect();
-*/
-/*
-                XMPPTCPConnectionConfiguration.Builder config = XMPPTCPConnectionConfiguration.builder();
-                config.setSecurityMode(ConnectionConfiguration.SecurityMode.required);
-                //For OLD STYLE SSL
-                config.setSecurityMode(ConnectionConfiguration.SecurityMode.enabled);
-                config.setUsernameAndPassword("username" + "@" + "pvp.net", "AIR_"+password);
-                config.setServiceName("pvp.net");
-                config.setHost(chat.na2.lol.riotgames.com);
-                config.setPort(5223);
-                config.setDebuggerEnabled(true);
-                //OLD STYLE SSL
-               
-                config.setSocketFactory(SSLSocketFactory.getDefault());
 
-                /*
-                try {
-                    SSLContext sc = SSLContext.getInstance("TLS");
-                    MemorizingTrustManager mtm = new MemorizingTrustManager(getBaseContext());
-                    sc.init(null, MemorizingTrustManager.getInstanceList(ctx), new SecureRandom());
-                    config.setCustomSSLContext(sc);
-                    config.setHostnameVerifier(mtm.wrapHostnameVerifier(new org.apache.http.conn.ssl.StrictHostnameVerifier()));
-                } catch (NoSuchAlgorithmException | KeyManagementException e) {
-                    throw new IllegalStateException(e);
-                }
-*/
-                XMPPTCPConnection  mConnection = new XMPPTCPConnection(config.build());
-                mConnection.setPacketReplyTimeout(10000);
 
-                try {
-                    mConnection.connect();
-                    mConnection.login();
-                } catch (SmackException | IOException | XMPPException e) {
-                    e.printStackTrace();
-                }
-
-                int a = 5;
-            } catch (Exception e) {
-
-                int a = 5;
-                int b = 5;
+                return null;
+            } catch (XMPPException e) {
+                e.printStackTrace();
+            } catch (SmackException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-
             return null;
         }
 
